9c39dfc5be2855750681df1c2cd8057eaf40267d
efem <qlkid@o2.pl>
2017-05-17 17:53:42 +0200

    Logging

--- src/main/java/pl/proj/controller/RecordController.java
+++ src/main/java/pl/proj/controller/RecordController.java
@@ -4,10 +4,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
 import pl.proj.dao.RecordDao;
 import pl.proj.domain.Record;
 import pl.proj.service.RecordService;
@@ -31,63 +30,93 @@ public class RecordController {
     @Autowired
     RecordService recordService;
 
-    @RequestMapping("putRecord/{distance:.+|,+}")
-    public String putRecord(@PathVariable String distance) {
-        recordService.save(new Record(new Date(), Double.parseDouble(distance.replaceAll(",","."))));
-        LOG.info("READ: " + distance + ", " + Double.parseDouble(distance.replaceAll(",",".")));
-        return "OK";
-    }
-
-    @RequestMapping("getRecords/{from:.+|,+}/{to:.+|,+}")
-    public List<Record> getRecordsBetweenDistance(
-            @PathVariable String from,
-            @PathVariable String to) {
-
-        LOG.info("RECORDS FROM DISTANCE: " + from + ", TO: " + to);
-
-        return recordService.findByDistanceBetween(
-                Double.parseDouble(from.replaceAll(",",".")),
-                Double.parseDouble(to.replaceAll(",",".")));
+    @RequestMapping("/")
+    public String start() {
+        return "<html><b>getAllRecords</b> - getting all records " +
+                "<br /><b>getRecordsByDistance/from/to</b> - getting records between from/to distance" +
+                "<br /><b>showRecordsByDistance?from=X&to=Y</b> - getting records between X and Y distance" +
+                "<br /><b>getRecordsByPage?pageNumber=X&pageSize=Y</b> - getting records with page number = X with page size = Y" +
+                "<br /><b>showRecordsByDate?from=X&to=Y</b> - getting records between X and Y dates (format: yyyy-MM-dd_HH-mm-ss)" +
+                "<br /><b>putRecord/X</b> - add a record with X distance</html>" ;
     }
 
     @RequestMapping("getAllRecords")
     public List<Record> getAllRecords() {
 
-        LOG.info("ALL RECORDS");
+        LOG.info("GET ALL RECORDS");
 
         return recordService.findAll();
     }
 
-    @RequestMapping("getLastRecords")
+    @RequestMapping("getRecordsByDistance/{from:.+|,+}/{to:.+|,+}")
+    public List<Record> getRecordsBetweenDistance(
+            @PathVariable String from,
+            @PathVariable String to) {
+
+        LOG.info("GET RECORDS BETWEEN DISTANCE (by PV): " + from + ", TO: " + to);
+
+        return recordService.findByDistanceBetween(
+                Double.parseDouble(from.replaceAll(",", ".")),
+                Double.parseDouble(to.replaceAll(",", ".")));
+    }
+
+    @RequestMapping("getRecordsByPage")
     public List<Record> getLastThree(
             @RequestParam int pageNumber,
             @RequestParam int pageSize) {
 
-        LOG.info("GET LAST RECORDS: " + ((pageNumber-1)*pageSize+1) + " - " + pageNumber*pageSize);
+        LOG.info("GET LAST RECORDS (by PV): " + ((pageNumber - 1) * pageSize + 1) + " - " + pageNumber * pageSize);
+
         Page<Record> page = recordService.getPageOfRecord(pageNumber, pageSize);
         return page.getContent();
     }
 
-    @RequestMapping("showRecords")
+
+    @RequestMapping("showRecordsByDistance")
     public List<Record> showRecordsBetweenDistance(
             @RequestParam String from,
             @RequestParam String to) {
 
-        LOG.info("RECORDS FROM DISTANCE: " + from + ", TO: " + to);
+        LOG.info("SHOW RECORDS BETWEEN DISTANCE (by RP): " + from + ", TO: " + to);
 
         return recordService.findByDistanceBetween(
-                Double.parseDouble(from.replaceAll(",",".")),
-                Double.parseDouble(to.replaceAll(",",".")));
+                Double.parseDouble(from.replaceAll(",", ".")),
+                Double.parseDouble(to.replaceAll(",", ".")));
     }
+
 
     @RequestMapping("showRecordsByDate")
     public List<Record> showRecordsBetweenDate(
             @RequestParam String from,
             @RequestParam String to) throws ParseException {
+
+        LOG.info("SHOW RECORDS BETWEEN DATES (by RP): " + from + ", TO: " + to);
+
         DateFormat format = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
-        LOG.info("RECORDS FROM DATE: " + from + ", TO: " + to);
         Date dateFrom = format.parse(from);
         Date dateTo = format.parse(to);
         return recordService.findByDateBetween(dateFrom, dateTo);
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public String handleAppException(Exception ex) {
+        return ex.getMessage();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @RequestMapping("putRecord/{distance:.+|,+}")
+    public String putRecord(@PathVariable String distance) {
+
+        try {
+            LOG.info("PUT RECORD (by PV): " + distance + ", " + Double.parseDouble(distance.replaceAll(",", ".")));
+            recordService.save(new Record(new Date(), Double.parseDouble(distance.replaceAll(",", "."))));
+            return "OK";
+        } catch (NumberFormatException nef) {
+            LOG.error("ERROR PARSING: " + nef);
+            //return "BAD VALUE: " + nef.getMessage();
+            return handleAppException(nef);
+        }
+
     }
 }
