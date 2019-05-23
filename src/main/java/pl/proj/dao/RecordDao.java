7d23eca7968bbd9ce736113c7b1b92b67a879702
yossar <efem@outlook.com>
2019-05-23 22:12:16 +0200

    Lombok v3

--- src/main/java/pl/proj/dao/RecordDao.java
+++ src/main/java/pl/proj/dao/RecordDao.java
@@ -8,9 +8,7 @@ import pl.proj.domain.Record;
 import java.util.Date;
 import java.util.List;
 
-/**
- * Created by msz on 09.05.17.
- */
+
 @Component
 public interface RecordDao extends JpaRepository<Record, Long> {
     List<Record> findByDistanceBetween(double from, double to);
