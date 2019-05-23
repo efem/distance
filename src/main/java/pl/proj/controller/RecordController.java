9a85ae79577bef2db937a4f7da90bba8f45f98b0
yossar <efem@outlook.com>
2019-05-23 22:10:17 +0200

    Lombok v2

--- src/main/java/pl/proj/controller/RecordController.java
+++ src/main/java/pl/proj/controller/RecordController.java
@@ -1,15 +1,12 @@
 package pl.proj.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 import pl.proj.domain.Record;
-import pl.proj.helper.DateTimeHelper;
 import pl.proj.service.RecordService;
 
 import java.text.DateFormat;
@@ -17,7 +14,7 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
-import java.util.TimeZone;
+
 
 
 @RestController
